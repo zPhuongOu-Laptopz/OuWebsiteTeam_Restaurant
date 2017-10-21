bcd37614808b7b0871fbb39dada57d6069dbf3bd
Nguyễn Ngọc Phương <1451010140Phuong@ou.edu.vn>
2017-10-21 22:14:21 +0700

    Update BookTable

---
+++ OuWebsiteTeam_RestaurantWebUI/Areas/Admin/Controllers/BookTableController.cs
@@ -0,0 +1,28 @@
+﻿using OuWebsiteTeam_RestaurantService.DBContext;
+using OuWebsiteTeam_RestaurantService.InterfaceEx.Module;
+using System;
+using System.Web.Mvc;
+
+namespace OuWebsiteTeam_RestaurantWebUI.Areas.Admin.Controllers
+{
+    public class BookTableController : Controller
+    {
+        private readonly IBookTable _context;
+
+        public BookTableController(IBookTable context)
+        {
+            this._context = context;
+        }
+        // GET: Admin/BookTable
+        public ActionResult List()
+        {
+            return View(this._context.GetAll());
+        }
+
+        public ActionResult Details(Guid id)
+        {
+            PdbBookTable mete = this._context.GetOne(id);
+            return View(mete);
+        }
+    }
+}
